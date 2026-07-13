7202fca1ed58577bbf20458b5e6dddc706ad04ee
TuqaMohd <tuqaocmt@gmail.com>
2026-07-13 09:22:33 +0400

    styling fixes

--- src/features/dashboard/components/DashboardIntro.tsx
+++ src/features/dashboard/components/DashboardIntro.tsx
@@ -2,9 +2,11 @@ export default function DashboardIntro() {
   return (
     <div className="mb-6">
       <h2 className="text-xl sm:text-2xl font-bold text-emeraldDark mb-2 text-center">JavaScript vs TypeScript</h2>
-      <p className="text-orange-950 text-sm sm:text-base text-center font-semibold">
-        TypeScript is JavaScript with types added. Click the buttons below to see
-        a few of the main differences between the two. Once done, test your knowledge in the Official Quest!
+      <p className="text-orange-950 text-sm sm:text-base text-center p-2 font-bold">
+        TypeScript is JavaScript with types added
+        <span className=" block text-orange-950 text-sm sm:text-base text-center font-bold">
+          Click the buttons below to see examples of some the main differences between the two.
+        </span>
       </p>
     </div>
   );
